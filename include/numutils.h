b1cefb5ee1c3806bba2f4bc30482e056588d8734
MartvanAssen <mart@vanassen.info>
2023-02-09 19:49:19 +0100

    get double and get int values from Number

--- include/numutils.h
+++ include/numutils.h
@@ -11,4 +11,8 @@ void bstd_assign_int(bstd_Number* number, int value);
 
 void bstd_assign_double(bstd_Number* number, double value);
 
+double bstd_get_double(bstd_Number* number);
+
+int64_t bstd_get_int(bstd_Number* number);
+
 #endif //CROSSOVER_NUMUTILS_H
--- src/numutils.c
+++ src/numutils.c
@@ -1,30 +1,41 @@
-//
-// Created by bruh on 11/28/22.
-//
 #include <math.h>
 #include <stdlib.h>
 #include "../include/numutils.h"
 
-void bstd_assign_int(bstd_Number* number, int value) {
+void bstd_assign_int(bstd_Number *number, int value) {
     bool positive = value >= 0;
 
-    int mag = (int)pow(10, number->length);
-    int mask = ((int)value / mag) * mag;
+    int mag = (int) pow(10, number->length);
+    int mask = ((int) value / mag) * mag;
     int cropped_value = abs(value) - mask;
 
     number->positive = positive;
     number->value = cropped_value;
 }
 
-void bstd_assign_double(bstd_Number* number, double value) {
+void bstd_assign_double(bstd_Number *number, double value) {
     bool positive = value >= 0;
 
     double mag1 = pow(10, number->length);
     double mag2 = pow(10, number->length - 1);
-    int shift = (int)(value * mag2);
-    int mask = (int)((double)shift / mag1) * mag1;
+    int shift = (int) (value * mag2);
+    int mask = (int) ((double) shift / mag1) * mag1;
     int cropped_value = abs(shift - mask);
 
     number->positive = positive;
     number->value = cropped_value;
+}
+
+double bstd_get_double(bstd_Number *number) {
+    int64_t value = number->positive ? (int64_t) number->value : (-1 * (int64_t) number->value);
+    double scale_divisor = pow(10, number->scale);
+    double double_value = value / scale_divisor;
+    return double_value;
+}
+
+int64_t bstd_get_int(bstd_Number *number) {
+    int64_t value = number->positive ? (int64_t) number->value : (-1 * (int64_t) number->value);
+    double scale_divisor = pow(10, number->scale);
+    int64_t int_value = (int64_t) (value / scale_divisor);
+    return int_value;
 }
