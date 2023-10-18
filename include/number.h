586409d3271e32f173a54a12a88947eade6d2dcc
Aime <m.a.ntagengerwa@gmail.com>
2023-10-18 16:51:25 +0200

    Remove unnecessary comments. Removed main.c

--- include/number.h
+++ include/number.h
@@ -3,14 +3,16 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define BSTD_NUMBER_MAX_LENGTH 19
+
 /**
- * todo: doc
+ * This is a complete representation of a BabyCobol numeric value.
  */
 typedef struct bstd_number_t {
-    uint64_t value;                                 // 8    0x00                        0x00
-    uint64_t scale;                                 // 8    0x08                        0x08
-    uint8_t length;                                 // 1    0x10                        0x10
-    bool isSigned; // todo: change bool to uint8_t  // 1    0x11
-    bool positive;                                  // 1    0x12 for a loength of 19.
+    uint64_t value;
+    uint64_t scale;
+    uint8_t length;
+    bool isSigned;
+    bool positive;
 } bstd_number;
 
